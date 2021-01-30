1cffd0caa1a729a2ed554dc0099a3c1f34e052d8
1jgrant <grantja92@gmail.com>
2021-01-30 15:04:30 +0000

    adds project to new system with new firebase db

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import * as firebase from "firebase";
 import "firebase/auth";
 import "firebase/firestore";
-var firebaseConfig = {
-  apiKey: "AIzaSyBPm46Yqq4vkJ0_hEj9-nsbtm3Z8XyUv6A",
-  authDomain: "activity-club-3dfcf.firebaseapp.com",
-  projectId: "activity-club-3dfcf",
-  storageBucket: "activity-club-3dfcf.appspot.com",
-  messagingSenderId: "100041602249",
-  appId: "1:100041602249:web:1d3e659dc288fe89cc8918",
-};
+import { devConfig } from "./fbConfig";
+
 // Below IF checks that we are not running any fb instance atm (avoids crashing)
 if (firebase.apps.length === 0) {
   // initializes fb
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(devConfig);
 }
 // React
 import React, { Component, useState, useEffect } from "react";
